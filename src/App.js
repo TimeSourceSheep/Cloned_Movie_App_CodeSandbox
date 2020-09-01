c9bc32caa1b5bdb37c77126d8ec8d26fa50359ca
Si-Won Yang <TimeSourceSheep@gmail.com>
2020-09-01 23:20:44 +0900

    Copied the progression so far ~p.179

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
-import "./styles.css";
+import axios from "axios";
+import Movie from "./Movie";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_page}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
+
+export default App;
