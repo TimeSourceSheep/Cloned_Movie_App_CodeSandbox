41000edcfd73677a9c9e96dd22fc7151c85542d2
Si-Won Yang <TimeSourceSheep@gmail.com>
2020-09-07 00:01:04 +0900

    Update 2020-09-07 (2) Added router stuff, about page, home page and navigator.

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>"Me smash"</span>
+      <span>- An Orc, unknown</span>
+    </div>
+  );
+}
+
+export default About;
